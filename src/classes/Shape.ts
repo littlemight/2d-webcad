5a33151fc752095d74137fde04c2b4b789ccf470
Michel Fang <mfang555@gmail.com>
2021-02-21 17:51:57 +0700

    add shape shader program

--- src/classes/Shape.ts
+++ src/classes/Shape.ts
@@ -1,10 +1,58 @@
 abstract class Shape {
+  program: WebGLProgram;
   canvas: HTMLCanvasElement;
   gl: WebGL2RenderingContext;
   constructor(canvas: HTMLCanvasElement, gl: WebGL2RenderingContext) {
     this.canvas = canvas;
     this.gl = gl;
+    this.program = this.createShaderProgram();
   }
+
+  createShaderProgram() {
+    const program = this.gl.createProgram();
+    if (!program) {
+      throw new Error("Error on creating program!");
+    }
+    this.gl.attachShader(
+      program,
+      this.createShader(
+        this.gl.VERTEX_SHADER,
+        `
+          precision mediump float;
+          uniform vec2 u_pos;
+          void main() {
+            gl_Position = vec4(u_pos, 0, 1);
+          }
+        `
+      )
+    );
+    this.gl.attachShader(
+      program,
+      this.createShader(
+        this.gl.FRAGMENT_SHADER,
+        `
+          precision mediump float;
+          uniform vec3 u_color;
+          void main() {
+            gl_FragColor = vec4(u_color, 1);
+          }
+        `
+      )
+    );
+    this.gl.linkProgram(program);
+    return program;
+  }
+
+  createShader(type: number, shaderCode: string): WebGLShader {
+    const shader = this.gl.createShader(type);
+    if (!shader) {
+      throw new Error("Error on creating shader!");
+    }
+    this.gl.shaderSource(shader, shaderCode);
+    this.gl.compileShader(shader);
+    return shader;
+  }
+
 }
 
 export default Shape;
