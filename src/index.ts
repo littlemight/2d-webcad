e8503e34e1a2d16bc1b3dcd95f3ec8e0bc8b485a
Michel Fang <mfang555@gmail.com>
2021-02-21 22:04:41 +0700

    add polygon example

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 import Application from "./classes/Application";
+import Polygon from "./classes/Polygon";
 
 const canvas = document.getElementById("gl-display") as HTMLCanvasElement;
 const gl = canvas.getContext("webgl2") as WebGL2RenderingContext;
@@ -8,3 +9,14 @@ if (!gl) {
 }
 
 const app = new Application(canvas, gl);
+const poly = new Polygon(canvas, gl, [1, 0, 0]);
+poly.points.push([-0.5, 0]);
+poly.points.push([0, -0.5]);
+poly.points.push([0.5, 0]);
+poly.points.push([0, 0.5]);
+app.shapeList.push(poly);
+const render = () => {
+  app.render();
+  window.requestAnimationFrame(render);
+};
+window.requestAnimationFrame(render);
