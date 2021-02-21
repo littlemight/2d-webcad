e54e518d1087f191254e64887dc6ddf4869f3563
Michel Fang <mfang555@gmail.com>
2021-02-21 22:02:48 +0700

    add render method

--- src/classes/Application.ts
+++ src/classes/Application.ts
@@ -3,11 +3,22 @@ import Shape from "./Shape";
 class Application {
   canvas: HTMLCanvasElement;
   gl: WebGL2RenderingContext;
-  shapes: Shape[];
+  shapeList: Shape[];
   constructor(canvas: HTMLCanvasElement, gl: WebGL2RenderingContext) {
     this.canvas = canvas;
     this.gl = gl;
-    this.shapes = [];
+    this.shapeList = [];
+
+    this.gl.viewport(0, 0, canvas.width, canvas.height);
+  }
+
+  render() {
+    this.gl.clearColor(1, 1, 1, 1);
+    this.gl.clear(this.gl.COLOR_BUFFER_BIT | this.gl.DEPTH_BUFFER_BIT);
+
+    for (const shape of this.shapeList) {
+      shape.render();
+    }
   }
 }
 
