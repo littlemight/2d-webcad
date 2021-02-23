ca0681e5ec0dde0660e2232673dee6d84f5ff63f
Michel Fang <mfang555@gmail.com>
2021-02-24 00:47:47 +0700

    refactor selectProgram setup to utils

--- src/classes/Application.ts
+++ src/classes/Application.ts
@@ -1,4 +1,9 @@
-import { createShader, rgbaToId, hexaToRGBA } from "../utils/utils";
+import {
+  createShader,
+  rgbaToId,
+  hexaToRGBA,
+  bulkProgramSetup,
+} from "../utils/utils";
 import Polygon from "./Polygon";
 import Rectangle from "./Rectangle";
 import Shape from "./Shape";
@@ -45,112 +50,7 @@ class Application {
     if (!selectProgram) {
       throw new Error("Failed when creating hitbox program!");
     }
-    this.gl.attachShader(
-      selectProgram,
-      createShader(
-        this.gl,
-        this.gl.VERTEX_SHADER,
-        `
-          precision mediump float;
-
-          attribute vec2 a_pos;
-
-          void main() {
-            gl_Position = vec4(a_pos, 0, 1);
-          }
-        `
-      )
-    );
-
-    this.gl.attachShader(
-      selectProgram,
-      createShader(
-        this.gl,
-        this.gl.FRAGMENT_SHADER,
-        `
-          precision mediump float;
-
-          uniform vec4 u_color;
-
-          void main() {
-            gl_FragColor = u_color;
-          }
-        `
-      )
-    );
-
-    this.gl.linkProgram(selectProgram);
-
-    const texBuf = this.gl.createTexture();
-    if (!texBuf) {
-      throw new Error("Failed creating texture");
-    }
-    this.gl.bindTexture(this.gl.TEXTURE_2D, texBuf);
-    this.gl.texParameteri(
-      this.gl.TEXTURE_2D,
-      this.gl.TEXTURE_MIN_FILTER,
-      this.gl.LINEAR
-    );
-    this.gl.texParameteri(
-      this.gl.TEXTURE_2D,
-      this.gl.TEXTURE_WRAP_S,
-      this.gl.CLAMP_TO_EDGE
-    );
-    this.gl.texParameteri(
-      this.gl.TEXTURE_2D,
-      this.gl.TEXTURE_WRAP_T,
-      this.gl.CLAMP_TO_EDGE
-    );
-
-    const depBuf = this.gl.createRenderbuffer();
-    if (!depBuf) {
-      throw new Error("Failed creating render buffer");
-    }
-    this.gl.bindRenderbuffer(this.gl.RENDERBUFFER, depBuf);
-
-    this.gl.bindTexture(this.gl.TEXTURE_2D, texBuf);
-    this.gl.texImage2D(
-      this.gl.TEXTURE_2D,
-      0,
-      this.gl.RGBA,
-      this.gl.canvas.width,
-      this.gl.canvas.height,
-      0,
-      this.gl.RGBA,
-      this.gl.UNSIGNED_BYTE,
-      null
-    );
-    this.gl.bindRenderbuffer(this.gl.RENDERBUFFER, depBuf);
-    this.gl.renderbufferStorage(
-      this.gl.RENDERBUFFER,
-      this.gl.DEPTH_COMPONENT16,
-      this.gl.canvas.width,
-      this.gl.canvas.height
-    );
-
-    const frameBuf = this.gl.createFramebuffer();
-    if (!frameBuf) {
-      throw new Error("Failed creating frame buffer");
-    }
-    this.frameBuf = frameBuf;
-
-    this.gl.bindFramebuffer(this.gl.FRAMEBUFFER, frameBuf);
-
-    // using the texture and depth buffer with frame buffer
-    this.gl.framebufferTexture2D(
-      this.gl.FRAMEBUFFER,
-      this.gl.COLOR_ATTACHMENT0,
-      this.gl.TEXTURE_2D,
-      texBuf,
-      0
-    );
-    this.gl.framebufferRenderbuffer(
-      this.gl.FRAMEBUFFER,
-      this.gl.DEPTH_ATTACHMENT,
-      this.gl.RENDERBUFFER,
-      depBuf
-    );
-
+    bulkProgramSetup(selectProgram, this.gl, this.frameBuf);
     return selectProgram;
   }
 
@@ -252,6 +152,7 @@ class Application {
 
   onMouseDown(point: Point) {
     this.mousePressed = true;
+    let addNewShape = false;
     if (this.mode === "selecting") {
       if (this.pixelId === undefined) {
         return;
@@ -277,14 +178,8 @@ class Application {
           [Math.random(), Math.random(), Math.random()],
           [Math.random(), Math.random(), Math.random()]
         );
-        poly.addPoint(this.mousePos);
-        poly.addPoint(this.mousePos);
-        this.shapeList.push(poly);
         this.drawingShape = poly;
-        this.selected = {
-          id: poly.id,
-          shape: poly,
-        };
+        addNewShape = true;
       }
     } else if (this.mode === "square") {
       if (this.drawingShape) {
@@ -297,14 +192,8 @@ class Application {
           [Math.random(), Math.random(), Math.random()],
           [Math.random(), Math.random(), Math.random()]
         );
-        rect.addPoint(this.mousePos);
-        rect.addPoint(this.mousePos);
-        this.shapeList.push(rect);
         this.drawingShape = rect;
-        this.selected = {
-          id: rect.id,
-          shape: rect,
-        };
+        addNewShape = true;
       }
     } else if (this.mode === "line") {
       if (this.drawingShape) {
@@ -317,16 +206,22 @@ class Application {
           [Math.random(), Math.random(), Math.random()],
           [Math.random(), Math.random(), Math.random()]
         );
-
-        line.addPoint(this.mousePos);
-        line.addPoint(this.mousePos);
-        this.shapeList.push(line);
         this.drawingShape = line;
-        this.selected = {
-          id: line.id,
-          shape: line,
-        };
+        addNewShape = true;
       }
+    }
+    if (
+      ["line", "square", "polygon"].includes(this.mode) &&
+      addNewShape &&
+      this.drawingShape
+    ) {
+      this.drawingShape.addPoint(this.mousePos);
+      this.drawingShape.addPoint(this.mousePos);
+      this.shapeList.push(this.drawingShape);
+      this.selected = {
+        id: this.drawingShape?.id,
+        shape: this.drawingShape,
+      };
     }
   }
 
--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -35,6 +35,110 @@ export function hexaToRGBA(hexadec: string) {
   const hexadec_r = hexadec[1] + hexadec[2];
   const hexadec_g = hexadec[3] + hexadec[4];
   const hexadec_b = hexadec[5] + hexadec[6];
-  const color_arr = [parseInt(hexadec_r,16)/255,parseInt(hexadec_g,16)/255,parseInt(hexadec_b,16)/255] as Color;
+  const color_arr = [
+    parseInt(hexadec_r, 16) / 255,
+    parseInt(hexadec_g, 16) / 255,
+    parseInt(hexadec_b, 16) / 255,
+  ] as Color;
   return color_arr;
+}
+
+export function bulkProgramSetup(
+  program: WebGLProgram,
+  gl: WebGL2RenderingContext,
+  _frameBuf: WebGLFramebuffer | null
+) {
+  gl.attachShader(
+    program,
+    createShader(
+      gl,
+      gl.VERTEX_SHADER,
+      `
+        precision mediump float;
+
+        attribute vec2 a_pos;
+
+        void main() {
+          gl_Position = vec4(a_pos, 0, 1);
+        }
+      `
+    )
+  );
+
+  gl.attachShader(
+    program,
+    createShader(
+      gl,
+      gl.FRAGMENT_SHADER,
+      `
+        precision mediump float;
+
+        uniform vec4 u_color;
+
+        void main() {
+          gl_FragColor = u_color;
+        }
+      `
+    )
+  );
+
+  gl.linkProgram(program);
+
+  const texBuf = gl.createTexture();
+  if (!texBuf) {
+    throw new Error("Failed creating texture");
+  }
+  gl.bindTexture(gl.TEXTURE_2D, texBuf);
+  gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR);
+  gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE);
+  gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE);
+
+  const depBuf = gl.createRenderbuffer();
+  if (!depBuf) {
+    throw new Error("Failed creating render buffer");
+  }
+  gl.bindRenderbuffer(gl.RENDERBUFFER, depBuf);
+
+  gl.bindTexture(gl.TEXTURE_2D, texBuf);
+  gl.texImage2D(
+    gl.TEXTURE_2D,
+    0,
+    gl.RGBA,
+    gl.canvas.width,
+    gl.canvas.height,
+    0,
+    gl.RGBA,
+    gl.UNSIGNED_BYTE,
+    null
+  );
+  gl.bindRenderbuffer(gl.RENDERBUFFER, depBuf);
+  gl.renderbufferStorage(
+    gl.RENDERBUFFER,
+    gl.DEPTH_COMPONENT16,
+    gl.canvas.width,
+    gl.canvas.height
+  );
+
+  const frameBuf = gl.createFramebuffer();
+  if (!frameBuf) {
+    throw new Error("Failed creating frame buffer");
+  }
+  _frameBuf = frameBuf;
+
+  gl.bindFramebuffer(gl.FRAMEBUFFER, frameBuf);
+
+  // using the texture and depth buffer with frame buffer
+  gl.framebufferTexture2D(
+    gl.FRAMEBUFFER,
+    gl.COLOR_ATTACHMENT0,
+    gl.TEXTURE_2D,
+    texBuf,
+    0
+  );
+  gl.framebufferRenderbuffer(
+    gl.FRAMEBUFFER,
+    gl.DEPTH_ATTACHMENT,
+    gl.RENDERBUFFER,
+    depBuf
+  );
 }
